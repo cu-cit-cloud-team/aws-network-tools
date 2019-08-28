1f4746c51d758118e3414a3574a3a7a730301a91
Paul Allen <pea1@cornell.edu>
2019-08-28 16:37:42 +0000

    add cloudwatch logs insights script

---
+++ cloudwatch-logs/README.md
@@ -0,0 +1,64 @@
+# cloudwatch-logs (ct-aws-network-tools)
+
+Tools for CloudWatch Logs
+
+## `insights-query.py`
+
+### Arguments
+
+argument (short) | argument (long) | description | example
+---- | ---- | ---- | ----
+`-v` | `--verbose` | print extra info | n/a
+`-g` | `--log-group` | log group name | flow-logs
+`-d` | `--days` | days to look back (default 0) | 1
+`-H` | `--hours` | hours to look back (default 1) | 1
+`-o` | `--output` | output file name (default 'out.csv') | results.csv
+`-f` | `--query-file` | file name holding query | flowlog.query
+
+`log-group` and `query-file` must be provided.
+
+### Examples
+
+```
+$ ./insights-query.py --log-group vpc-5ffce339 --query flowlog.query
+============================================================
+Configuration:
+verbose: False
+log group: vpc-5ffce339
+output file: out.csv
+start time: 2019-08-28 11:32:10.816189
+end time:   2019-08-28 12:32:10.816187
+query:
+--------------------------------------------------------------------------------
+# Generic flow log query
+# Top 10 byte transfers by source and destination IP addresses
+stats sum(bytes) as bytesTransferred by srcAddr, dstAddr
+| sort bytesTransferred desc
+| limit 10
+--------------------------------------------------------------------------------
+2019-08-28 12:32:16.110410: query 645f3613-e49e-4013-9765-51e98f7267f8 Complete
+Records Matched: 714645 Records Scanned: 795993 Bytes Scanned: 109213205
+Records Retrieved: 10
+$ cat out.csv
+srcAddr,dstAddr,bytesTransferred
+10.92.164.132,10.92.164.138,947989460
+10.92.164.138,10.92.164.132,929375768
+169.46.59.7,10.92.164.53,524243911
+10.92.164.236,10.92.164.123,479525508
+10.92.164.236,10.92.164.14,444210645
+10.92.164.53,169.46.59.7,433115903
+10.92.164.53,10.92.164.178,273205100
+10.92.164.53,10.92.164.136,271683383
+169.46.59.7,10.92.164.178,266975233
+10.92.82.193,10.92.164.236,247657989
+$
+```
+
+### Limits
+
+At present AWS API/SDK appears to limit the total number of records returned to 1,000, regardless of how many records result from the query.
+
+### Future Enhancements
+
+- Support absolute dates, instead of just time periods relative to current time
+
---
+++ cloudwatch-logs/insights-query.py
@@ -0,0 +1,131 @@
+#!/usr/bin/env python3
+"""
+Pull flow log data from CloudWatch Logs using Logs Insight
+"""
+import sys
+from pprint import pprint
+import argparse
+import ipaddress
+import socket
+import boto3
+import datetime
+import time
+import csv
+
+
+def convert_results(list):
+    """
+    Convert query results into a more concise form.
+
+    Example data:
+    [[{'field': 'srcAddr', 'value': '40.121.87.244'},
+      {'field': 'dstAddr', 'value': '10.92.164.23'},
+      {'field': 'srcPort', 'value': '7901'},
+      {'field': 'dstPort', 'value': '1521'},
+      {'field': 'protocol', 'value': '6'},
+      {'field': 'sumBytes', 'value': '3243845'}],
+     [{'field': 'srcAddr', 'value': '40.121.87.244'},
+      {'field': 'dstAddr', 'value': '10.92.164.23'},
+      {'field': 'srcPort', 'value': '1460'},
+      {'field': 'dstPort', 'value': '1521'},
+      {'field': 'protocol', 'value': '6'},
+      {'field': 'sumBytes', 'value': '1982005'}]]
+    """
+    result = []
+    for record in list:
+        new_record = {}
+        for item in record:
+            field = item['field']
+            value = item['value']
+            new_record[field] = value
+        result.append(new_record)
+    return result
+
+def main(argv):
+
+    parser = parser = argparse.ArgumentParser(description='pull flow log data from CloudWatch Logs using Logs Insight')
+    parser.add_argument('-v', '--verbose', default=False, action='store_true', help="show more info")
+    parser.add_argument('-g', '--log-group', required=True, help="log group name")
+    parser.add_argument('-d', '--days', required=False, default='0', help="look back days")
+    parser.add_argument('-H', '--hours', required=False, default='1', help="look back hours")
+    parser.add_argument('-o', '--output', required=False, default='out.csv', help="output file name")
+    parser.add_argument('-f', '--query-file', required=True, help="query input file name")
+    args = parser.parse_args()
+
+    verbose = args.verbose
+    log_group_name = args.log_group
+    end_time =  datetime.datetime.now()
+    start_time = datetime.datetime.now() - datetime.timedelta(days=int(args.days), hours=int(args.hours))
+    output_file = args.output
+    query_file = args.query_file
+
+    with open(query_file, 'r') as file:
+        query = file.read()
+
+    print("============================================================")
+    print("Configuration:")
+    print(f"verbose: {str(verbose)}")
+    print(f"log group: {log_group_name}")
+    print(f"output file: {output_file}")
+    print(f"start time: {start_time}")
+    print(f"end time:   {end_time}")
+    print('query:\n'+'-'*80)
+    print(query)
+    print('-'*80)
+
+
+    ec2_client = boto3.client('ec2')
+    logs_client = boto3.client('logs')
+
+    response = logs_client.start_query(
+        logGroupName=log_group_name,
+        # logGroupNames=[
+        #     'string',
+        # ],
+        startTime=int(start_time.timestamp()),
+        endTime=int(end_time.timestamp()),
+        queryString=query,
+        # limit=20
+    )
+    if verbose:
+        pprint(response)
+    query_id = response['queryId']
+
+    status = 'Scheduled'
+    while status in ['Running', 'Scheduled']:
+        time.sleep(5)
+        response = logs_client.describe_queries(
+            logGroupName=log_group_name,
+            # status='Complete'|'Failed'|'Cancelled',
+            maxResults=999,
+            # nextToken='string'
+        )
+        target_query = None
+        status = None
+        for q in response['queries']:
+            if q['queryId'] == query_id:
+                target_query = q
+                status = q['status']
+                break
+        print(f"{str(datetime.datetime.now())}: query {query_id} {status}")
+
+    response = logs_client.get_query_results(
+        queryId=query_id
+    )
+    if verbose:
+        pprint(response)
+    response['statistics']['recordsMatched']
+    print(f"Records Matched: {int(response['statistics']['recordsMatched'])} Records Scanned: {int(response['statistics']['recordsScanned'])} Bytes Scanned: {int(response['statistics']['bytesScanned'])}")
+    result = convert_results(response['results'])
+    if verbose:
+        pprint(result)
+    print(f"Records Retrieved: {len(result)}")
+    if len(result) > 0:
+        with open(output_file, mode='w') as csv_file:
+            writer = csv.DictWriter(csv_file, fieldnames=result[0].keys())
+            writer.writeheader()
+            for record in result:
+                writer.writerow(record)
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
